10e41976cd3f595eacc5cf4f77977532b958c0a3
matthewjrogers <33400922+matthewjrogers@users.noreply.github.com>
2023-04-06 14:15:05 -0400

    enabling fields in airtable obj

--- R/airtable.R
+++ R/airtable.R
@@ -5,6 +5,7 @@
 #' @param table Table name in Airtable
 #' @param base Airtable base containing table. A base functions like a schema in a traditional database. You can retrieve the base ID from the API documentation.
 #' @param view Optional view of data to read
+#' @param fields An optional `airtable_fields_schema` object. Automatically populated in tables created by `airtable_base()`
 #' @param api_url API endpoint to connect to. Can be changed for API integrations that require custom endpoint
 #' @param api_version Version of API to use. Defaults to 0 (the current version as of Fall 2021)
 #'
@@ -16,11 +17,11 @@
 #' table <- airtable("Table 1", "appXXXXXXXXXXXXX")
 #' }
 
-airtable <- function(table, base, view = NULL, api_url = 'https://api.airtable.com', api_version = 0){
-
-  atbl <- new_airtable(table, base, view, api_url, as.integer(api_version))
-
+airtable <- function(table, base, view = NULL, fields = list(), api_url = 'https://api.airtable.com', api_version = 0){
+  
+  atbl <- new_airtable(table, base, view, fields, api_url = api_url, api_version = as.integer(api_version))
+  
   validate_airtable(atbl)
-
+  
   atbl
 }
