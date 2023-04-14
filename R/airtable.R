e03b7acd352727335ef443022d6cce409e22aeb0
Eli Pousson <eli.pousson@gmail.com>
2023-04-13 22:54:27 -0400

    docs: revise airtable documentation
    - docs: revise airtable documentation
    - feat: remove str method for airtable objects

--- R/airtable.R
+++ R/airtable.R
@@ -3,19 +3,21 @@
 #' Creates an S3 airtable object, which serves as a pointer for rairtable
 #' functions
 #'
-#' @param table Table name in Airtable
-#' @param base Airtable base containing table. A base functions like a schema in
-#'   a traditional database. You can retrieve the base ID from the API
-#'   documentation.
-#' @param view Optional view of data to read
+#' @param table Airtable table ID or name. Table ID values start with "tbl". If
+#'   table is an Airtable URL matching the pattern
+#'   "https://airtable.com/{baseID}/{tableIdOrName}/{viewId}" the URL is parsed
+#'   to supply the values for base and table.
+#' @param base Airtable base ID. Base ID values start with "app". Optional if
+#'   table is a URL containing a base ID or required otherwise.
+#' @param view Optional Airtable view ID. View ID values starts with "viw".
 #' @param fields An optional `airtable_fields_schema` object. Automatically
-#'   populated in tables created by `airtable_base()`
-#' @param api_url API endpoint to connect to. Can be changed for API
-#'   integrations that require custom endpoint
-#' @param api_version Version of API to use. Defaults to 0 (the current version
-#'   as of Fall 2021)
+#'   populated in tables created by `airtable_base()`.
+#' @inheritParams airtable_request
+#' @inheritParams check_airtable_obj
 #'
-#' @return An airtable object
+#' @return An `airtable` class list of table and fields stored as values of the
+#'   list and the base, view, and request_url stored as attributes.
+#'
 #' @export
 #'
 #' @examples
@@ -89,59 +91,6 @@ build_airtable_obj <- function(table,
   atbl
 }
 
-#' Does x inherit the airtable class?
-#'
-#' @noRd
-is_airtable_obj <- function(x) {
-  inherits(x, "airtable")
-}
-
-#' Validate a new airtable object
-#'
-#' @noRd
-check_airtable_obj <- function(x,
-                               require_url = TRUE,
-                               require_table = TRUE,
-                               require_view = FALSE,
-                               require_fields = FALSE,
-                               call = caller_env()) {
-  if (!is_airtable_obj(x)) {
-    cli_abort("The provided airtable object is not of class `airtable`")
-  }
-
-  check_string(attr(x, "base"), call = call)
-
-  if (require_table) {
-    check_string(x$table, call = call)
-  }
-
-  if (require_url) {
-    url <- attr(x, "request_url")
-
-    check_airtable_api_url(
-      url,
-      require_table = require_table,
-      require_view = require_view
-    )
-  }
-
-  if (require_view) {
-    check_string(attr(x, "view"), call = call)
-  }
-
-  if (require_fields) {
-    fields <- x$fields
-
-    if (!is.list(fields)) {
-      cli_abort("The fields of the provided Airtable object is not a list")
-    }
-
-    if (!inherits(fields, "airtable_fields_schema")) {
-      cli_abort("The fields of the provided Airtable object must be of class `airtable_fields_schema`")
-    }
-  }
-}
-
 #' print method for an airtable object
 #'
 #' @keywords internal
@@ -152,14 +101,4 @@ print.airtable <- function(x, ...) {
     cat("   View: ", attr(x, "view"), "\n", sep = "")
   }
   cat("   Base: ", attr(x, "base"), "\n", sep = "")
-}
-
-
-#' str method for an airtable object
-#'
-#' @keywords internal
-#' @export
-str.airtable <- function(object, ...) {
-  view <- ifelse(is.null(attr(object, "view")), "", paste0('."', attr(object, "view"), '"'))
-  cat(" Airtable: ", object$table, view, " @ ", attr(object, "base"), "\n", sep = "")
 }
---
+++ R/is_airtable_obj.R
@@ -0,0 +1,63 @@
+#' Test or check if an object is an airtable object
+#'
+#' @param x Object to test or check.
+#' @keywords internal
+#' @export
+is_airtable_obj <- function(x) {
+  inherits(x, "airtable")
+}
+
+#' @name check_airtable_obj
+#' @rdname is_airtable_obj
+#' @param require_url If `TRUE` (default), check that x includes a "request_url"
+#'   attribute that is a valid Airtable API url.
+#' @inheritParams check_airtable_api_url
+#' @param require_fields If `TRUE`, check that x includes a "fields" value that
+#'   is a list with the `airtable_fields_schema` class. Defaults to `FALSE`.
+#' @keywords internal
+#' @export
+check_airtable_obj <- function(x,
+                               require_url = TRUE,
+                               require_table = TRUE,
+                               require_view = FALSE,
+                               require_fields = FALSE,
+                               call = caller_env()) {
+  if (!is_airtable_obj(x)) {
+    cli_abort("{.arg airtable} must be an {.cls airtable} class object.")
+  }
+
+  check_string(attr(x, "base"), call = call)
+
+  if (require_table) {
+    check_string(x$table, call = call)
+  }
+
+  if (require_url) {
+    url <- attr(x, "request_url")
+
+    check_airtable_api_url(
+      url,
+      require_table = require_table,
+      require_view = require_view,
+      call = call
+    )
+  }
+
+  if (require_view) {
+    check_string(attr(x, "view"), call = call)
+  }
+
+  if (require_fields) {
+    fields <- x[["fields"]]
+
+    if (!is.list(fields)) {
+      cli_abort("{.arg airtable} must include a list of fields.")
+    }
+
+    if (!inherits(fields, "airtable_fields_schema")) {
+      cli_abort(
+        "{.arg airtable} must have {.cls airtable_fields_schema} class fields."
+      )
+    }
+  }
+}
