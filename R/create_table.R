2cfc1ed19261ec4e73d61ebd565b24dfe54941a8
Eli Pousson <eli.pousson@gmail.com>
2023-06-15 14:23:21 -0400

    refactor: add validation for make_table_model + create_table

--- R/create_table.R
+++ R/create_table.R
@@ -1,6 +1,7 @@
 #' Create or update an Airtable table
 #'
-#' Create or update an Airtable table or create a table model. At least one of
+#' Create or update an Airtable table or create a table model. [create_table()]
+#' requires both a name and fields (or a model with a name and fields). Either a
 #' name or description must be provided for [update_table()].
 #'
 #' Find more information on the create table API:
@@ -55,30 +56,41 @@ create_table <- function(airtable = NULL,
                          fields = NULL,
                          token = NULL,
                          ...) {
-  model <- model %||%
-    make_table_model(
+  if (is_null(model)) {
+    model <- make_table_model(
       name = name,
       description = description,
       fields = fields
     )
+  } else {
+    check_table_model(model)
+  }
 
-  cli_progress_step(
-    "Creating a new table: {.val {model[['name']]}}"
+  base <- get_base_id(
+    airtable = airtable,
+    ...
   )
 
   resp <- request_airtable_meta(
-    airtable = airtable,
-    ...,
+    base = base,
     meta = "create_table",
     data = model,
     token = token
   )
 
+  cli_progress_step(
+    "Creating {.val {model[['name']]}} table with primary field
+    {.val {model[['fields']][[1]][['name']]}} and {length(model[['fields']])-1}
+    more field{?s}"
+  )
+
   body <- httr2::resp_body_json(resp)
 
+  base_url <- getOption("rairtable.base_url", "https://airtable.com")
+  table_url <- glue("{base_url}/{base}/{body[['id']]}")
 
   cli_progress_step(
-    "Table ID {.val {body[['id']]}} created."
+    "New table created at {.url {table_url}}"
   )
 
   invisible(body)
@@ -129,12 +141,56 @@ make_table_model <- function(name = NULL,
                              fields = NULL,
                              ...,
                              call = caller_env()) {
+  check_string(name, allow_empty = FALSE, call = call)
+  # TODO: Double-check if table descriptions have the same constraints as field
+  # descriptions
+  check_field_description(description = description, allow_null = TRUE, call = call)
+
+  fields <- make_field_array(fields, arg = "fields", call = call)
+  check_field_array(fields, call = call)
+
   vctrs::list_drop_empty(
     list(
       name = name,
       description = description,
-      fields = make_field_array(fields, arg = "fields", call = call)
+      fields = fields
     )
+  )
+}
+
+#' Check table model
+#'
+#' @noRd
+check_table_model <- function(model, allow_null = FALSE, call = caller_env()) {
+  if (allow_null && is_null(model)) {
+    return(invisible(NULL))
+  }
+
+  check_list(model, allow_null = allow_null, call = call)
+
+  if (!is_string(model[["name"]])) {
+    cli_abort(
+      "{.arg model} must include a table name.",
+      call = call
+    )
+  }
+
+  if (!is_list(model[["fields"]])) {
+    cli_abort(
+      "{.arg model} must include a field list.",
+      call = call
+    )
+  }
+
+  try_fetch(
+    check_field_array(model[["fields"]], call = call),
+    error = function(cnd) {
+      cli_abort(
+        "{.arg model} fields must be a valid field array.",
+        parent = cnd,
+        call = call
+      )
+    }
   )
 }
 
@@ -153,14 +209,14 @@ make_field_array <- function(fields = NULL,
     table <- tables[tables[["name"]] %in% table | tables[["id"]] %in% table, ]
   }
 
+  if (has_name(table, "fields")) {
+    values <- table[["fields"]][[1]][["name"]]
+  }
+
   field_array <- lapply(
     X = make_field_list(fields, arg = arg, call = call),
     FUN = make_field_config
   )
-
-  if (has_name(table, "fields")) {
-    values <- table[["fields"]][[1]][["name"]]
-  }
 
   if (is_null(values)) {
     return(field_array)
@@ -172,7 +228,7 @@ make_field_array <- function(fields = NULL,
 
     if (any(nm_in_values)) {
       cli_abort(
-        c("{.arg fields} must not include any existing field names.",
+        c("{.arg {arg}} can't use any existing field names.",
           "*" = "Rename field{?s} {.val {field_array_nm[nm_in_values]}}
           in {.arg fields} or target {.arg table}."
         ),
@@ -183,6 +239,33 @@ make_field_array <- function(fields = NULL,
 
   field_array
 }
+
+#' Is x a field array list?
+#'
+#' This function could likely be combined with check_airtable_fields_schema()
+#'
+#' @noRd
+check_field_array <- function(fields,
+                              allow_null = FALSE,
+                              allow_names = c("type", "name", "description", "options"),
+                              call = caller_env()) {
+  if (allow_null && is_null(fields)) {
+    return(invisible(NULL))
+  }
+
+  check_list(fields, allow_null = allow_null, call = call)
+
+  fields_names <- unlist(lapply(fields, names))
+
+  if (!all(fields_names %in% allow_names)) {
+    cli_abort(
+      "{.arg fields} must only include valued named {.val {allow_names}},
+      not {.val {fields_names[!(fields_names %in% allow_names)]}}.",
+      call = call
+    )
+  }
+}
+
 
 #' Make a table config object
 #'
