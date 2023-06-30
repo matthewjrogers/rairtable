4d616430ba3024247ef8a4b5e62a0d3ea5edf838
Eli Pousson <eli.pousson@gmail.com>
2023-06-29 23:21:11 -0400

    refactor: rename field_types.R + add field_name_match

---
+++ R/field_match.R
@@ -0,0 +1,33 @@
+#' Match a variable to a vector of field_types
+#'
+#' @noRd
+field_type_match <- function(type,
+                             error_arg = caller_arg(type),
+                             error_call = caller_env()) {
+  arg_match(
+    type,
+    values = field_types[["type"]],
+    error_arg = error_arg,
+    error_call = error_call
+  )
+}
+
+#' Match a field name to a vector of names from a table model
+#'
+#' @noRd
+field_name_match <- function(nm,
+                             values = NULL,
+                             model = NULL,
+                             multiple = TRUE,
+                             error_arg = caller_arg(nm),
+                             error_call = caller_env()) {
+  values <- values %||% names_at(model[["fields"]])
+
+  arg_match(
+    nm,
+    values = values,
+    multiple = multiple,
+    error_arg = error_arg,
+    error_call = error_call
+  )
+}
